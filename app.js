14c39acfc77bc1e84d324a206fb8c2f81812466e
rbehera <rbehera@asu.edu>
2021-02-07 17:51:46 +0530

    use of middleware in express

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 // Register View Engine
 app.set("view engine", "ejs");
 
 app.listen(3000);
+
+// moddleware & static files
+app.use(express.static("public"));
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
