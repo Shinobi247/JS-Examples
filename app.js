990d55e0edb2c581f47393d3cfb08164ef8235d6
rbehera <rbehera@asu.edu>
2021-02-08 11:54:23 +0530

    Added mongoose to connect to mongo db

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+// express app
 const app = express();
+
+// connect to mongodb
+// const dbURI =
+//   "mongodb+srv://rahul:rahul247@cluster0.10afx.mongodb.net/node-tuts?retryWrites=true&w=majority";
+const dbURI =
+  "mongodb://rahul:rahul247@cluster0-shard-00-00.10afx.mongodb.net:27017,cluster0-shard-00-01.10afx.mongodb.net:27017,cluster0-shard-00-02.10afx.mongodb.net:27017/node-tuts?ssl=true&replicaSet=atlas-10oidf-shard-0&authSource=admin&retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("Connected to DB");
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
 
 // Register View Engine
 app.set("view engine", "ejs");
-
-app.listen(3000);
 
 // moddleware & static files
 app.use(express.static("public"));
